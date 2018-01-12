4e744ec2bff6e3c7638dcb6086144d2dae375db1
Eike Spielberg <Eike.Spielberg@uni-due.de>
2018-01-12 14:32:02 +0100

    added ajp support for embedded tomcat server

--- src/main/java/unidue/ub/services/gateway/GatewayApplication.java
+++ src/main/java/unidue/ub/services/gateway/GatewayApplication.java
@@ -4,18 +4,46 @@
 */
 package unidue.ub.services.gateway;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
 public class GatewayApplication {
 
+	@Value("${tomcat.ajp.port}")
+	int ajpPort;
+
+	@Value("${tomcat.ajp.enabled}")
+	boolean tomcatAjpEnabled;
+
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+
+		if (tomcatAjpEnabled) {
+			Connector ajpConnector = new Connector("AJP/1.3");
+			ajpConnector.setPort(ajpPort);
+			ajpConnector.setSecure(false);
+			ajpConnector.setAllowTrace(false);
+			ajpConnector.setScheme("http");
+			tomcat.addAdditionalTomcatConnectors(ajpConnector);
+		}
+
+		return tomcat;
 	}
 
 }
